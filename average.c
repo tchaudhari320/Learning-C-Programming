6b26eb1b340ab1c73edffbccd3336dbba79cbf25
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-09-17 03:19:47 +0530

    Add files via upload

---
+++ average.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int a,b,c;
+	printf("enter number 1");
+	scanf("%d", &a);
+	printf("enter number 2");
+	scanf("%d", &b);
+	c = (a+b)/2;
+	printf("\naverage of c=%d", c);
+	scanf("%d", &c);
+}
