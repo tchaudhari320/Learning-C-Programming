9e4b00f4187ca9554d941e41902ebad73451adaf
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-09-17 03:24:00 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int r, A;
+	printf("enter radius");
+	scanf("%d", &r);
+	A = 3.14*r*r;
+	printf("\narea of circle=%d", A);
+	scanf("%d", &A);
+}
