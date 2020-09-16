684c9bab18e4576d06ea69303a1b125c8e33b8ac
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-09-17 03:28:46 +0530

    Add files via upload

---
+++ ctof.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	float c, f;
+	printf("enter celcius");
+	scanf("%f", &c);
+	f = 1.8*c+32;
+	printf("\nfarheniet=%f", f);
+	scanf("%d", &f);
+}
