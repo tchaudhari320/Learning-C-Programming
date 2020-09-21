63656e6f5ea54644a097e743dc20b37f5efd9c22
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-09-22 00:15:57 +0530

    Add files via upload

---
+++ covid.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	if(-~-1)
+		printf("COVID");
+	if((~7 & 0x000f)==8)
+		printf("19");
+	printf("*");
+}
+
---
+++ infinitewelcome.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+	for(;;)
+		printf("Welcome");
+	return 0;
+}
