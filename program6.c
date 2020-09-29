0f232bbd802103ccff0790ae0603e300f4b69a8e
Arpit Bhushan Sharma <42774305+arpit1920@users.noreply.github.com>
2020-09-29 10:09:50 +0530

    Add files via upload

---
+++ program6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+	float f=0.7;
+	double d=0.7;
+	printf("%d \t %d \t %d \t", sizeof(f), sizeof(d), sizeof(0.7));
+	return 0;
+}
